b910ff2a6f4a797dd19a6c7c8fda8500f0f2f2da
matdebski <95379996+matdebski@users.noreply.github.com>
2025-05-23 14:17:24 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,9 +8,41 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "transcriberstorage1"
+  name                     = "transcribersa123"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "raw" {
+  name                  = "raw"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "audio" {
+  name                  = "audio"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "transcripts" {
+  name                  = "transcripts"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_servicebus_namespace" "sb" {
+  name                = "transcriberbus"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Basic"
+}
+
+resource "azurerm_servicebus_queue" "queue" {
+  name                = "transcriberqueue"
+  namespace_id        = azurerm_servicebus_namespace.sb.id
+  max_delivery_count  = 10
+  enable_partitioning = true
 }
