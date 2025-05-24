7f7160e0cfb1956e305c0315ed5553a6fce0724e
matdebski <mateusz.debski@student.put.poznan.pl>
2025-05-25 00:39:51 +0200

    Initial terraform

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.29.0"
+    }
+  }
+}
+
+provider "azurerm" {
+features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "transcriberapp-rg"
+  location = "East Europe"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "transcriberstorage1"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
