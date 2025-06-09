8be79ef57293ebcf41459a363ada9ebeb13f6f1c
matdebski <mateusz.debski@student.put.poznan.pl>
2025-06-09 12:12:27 +0200

    Comment local connection string

--- backend/UploadFunction/function_app.py
+++ backend/UploadFunction/function_app.py
@@ -18,7 +18,7 @@ connection_string = (
     f"EndpointSuffix=core.windows.net"
 )
 
-connection_string="AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;"
+#connection_string="AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;"
 
 blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
