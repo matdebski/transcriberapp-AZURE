272bdf222b6b09da48f6319111e97224b4aabfd0
matdebski <mateusz.debski@student.put.poznan.pl>
2025-06-16 08:31:58 +0200

    Fix error in storage configuration

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -3,7 +3,16 @@ resource "azurerm_storage_account" "storage" {
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "LRS" 
+  blob_properties {
+    cors_rule {
+    allowed_origins    = ["*"]
+    allowed_methods    = ["GET"]
+    allowed_headers    = ["*"]
+    exposed_headers    = ["*"]
+    max_age_in_seconds = 3600
+    }
+  }
 }
 
 resource "azurerm_storage_container" "input" {
@@ -15,12 +24,5 @@ resource "azurerm_storage_container" "input" {
 resource "azurerm_storage_container" "output" {
   name                  = "output-${var.project_name}"
   storage_account_id  = azurerm_storage_account.storage.id
-  container_access_type = "blob"
-    cors_rule {
-    allowed_origins    = ["*.azurestaticapps.net"]
-    allowed_methods    = ["GET"]
-    allowed_headers    = ["*"]
-    exposed_headers    = ["*"]
-    max_age_in_seconds = 3600
-  }
+  container_access_type = "blob" 
 }
