7324d7ee57cf570686042b634a8baca29d410e10
matdebski <95379996+matdebski@users.noreply.github.com>
2026-01-22 10:13:08 +0100

    Update function_app.py

--- backend/UploadFunction/function_app.py
+++ backend/UploadFunction/function_app.py
@@ -37,8 +37,6 @@ connection_string = (
     f"EndpointSuffix=core.windows.net"
 )
 
-#connection_string="AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;"
-
 blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
 @app.function_name('UploadFunction')
@@ -84,3 +82,4 @@ def upload(req: func.HttpRequest) -> func.HttpResponse:
             status_code=500,
             mimetype="application/json"
         )
+
